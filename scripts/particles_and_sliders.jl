294aa2626d1dedc1577378eea0a9dcaaa4c9198f
Marcin <marcin.mider@gmail.com>
2019-07-04 19:40:56 +0200

    added graph tracking coordinates of a single particle and plotting them in 'X^{[i]}_t vs t' format

--- scripts/particles_and_sliders.jl
+++ scripts/particles_and_sliders.jl
@@ -1,10 +1,6 @@
 using Makie, Makie.AbstractPlotting
 using Makie.AbstractPlotting: textslider
-
-using StaticArrays
-
 using Random
-using Distributions
 using Colors
 
 RECORD = false
@@ -14,12 +10,12 @@ sq_dt = sqrt(dt)
 
 
 # One slider for each parameter
-s1, ϵ = textslider(0.001f0:0.001f0:3.0f0, "ϵ", start = 0.1)
-s2, s = textslider(-5.0f0:0.001f0:5.0f0, "s", start = 0.0)
-s3, γ = textslider(-3.0f0:0.001f0:3.0f0, "γ", start = 1.5)
-s4, β = textslider(-3.0f0:0.001f0:3.0f0, "β", start = 0.8)
-s5, σ₁ = textslider(0.0f0:0.001f0:1.0f0, "σ₁", start = 0.0)
-s6, σ₂ = textslider(0.0f0:0.001f0:3.0f0, "σ₂", start = 0.3)
+s1, σ₂ = textslider(0.0f0:0.001f0:3.0f0, "σ₂", start = 0.3)
+s2, σ₁ = textslider(0.0f0:0.001f0:1.0f0, "σ₁", start = 0.0)
+s3, β = textslider(-3.0f0:0.001f0:3.0f0, "β", start = 0.8)
+s4, γ = textslider(-3.0f0:0.001f0:3.0f0, "γ", start = 1.5)
+s5, s = textslider(-5.0f0:0.001f0:5.0f0, "s", start = 0.0)
+s6, ϵ = textslider(0.001f0:0.001f0:3.0f0, "ϵ", start = 0.1)
 
 # Define the dynamics of the process
 function drift(x::Point2f0, ϵ=0.1, s=0.0, γ=1.5, β=0.8)
@@ -42,42 +38,76 @@ function update!(t, x, y, jump)
     end
     y
 end
+update!(x, y, jump = indentity) = update!(NaN, x, y, jump)
 
-# Define traces zapping around the space & initialise them
+function update(x)
+    k = Point2f0(x)
+    (k + drift(k, to_value.(drift_param)...)*dt
+       + sq_dt * vola(k, to_value.(vola_param)...).* randn(2))
+end
+
+
+# Define traces zapping around the space
 trace_len = 250
 num_pts = 2500
 starting_pts = [Point2f0(2*R*rand(2).-R) for i in 1:num_pts]
 xraw = [copy(starting_pts) for i in 1:trace_len]
 x = [Node(xraw[i]) for i in 1:trace_len]
 col = [Node(RGBA{Float32}(0.0, 0.0, 0.0, 0.0)) for i in 1:trace_len]
-c = 1
 ms = 0.02
-#i = 1;
 
 limits = FRect(-R, -R, 2R, 2R)
-particles_canvas = Scene(resolution=(800,800), limits=limits, backgroundcolor = RGB{Float32}(0.04, 0.11, 0.22))
+bg_col = RGB{Float32}(0.04, 0.11, 0.22)
+particles_canvas = Scene(resolution=(800,800), limits=limits, backgroundcolor = bg_col)
 for i in randperm(trace_len)
     scatter!(particles_canvas, x[i], color = col[i], markersize = ms, #=show_axis = true,limits=limits,=#  glowwidth = 0.005, glowcolor = :white)
 end
-#update_cam!(p, limits)
-axis = particles_canvas[Axis]
-axis[:grid, :linewidth] =  (1, 1)
-axis[:grid, :linecolor] = (RGBA{Float32}(0.5, 0.7, 1.0, 0.5),RGBA{Float32}(0.5, 0.7, 1.0, 0.5))
-axis[:names][:textsize] = (0.0,0.0)
-axis[:ticks, :textcolor] = (RGBA{Float32}(0.5, 0.7, 1.0, 0.8),RGBA{Float32}(0.5, 0.7, 1.0, 0.8))
 
-parent = Scene(resolution = (1000, 800))
-vbox(hbox(s1, s2, s3, s4, s5, s6), particles_canvas, parent = parent)
+
+# Define graph tracing history of each coordinate of a single particle
+T = 8.0
+frame_time = collect(0:dt:T)
+frame_start = 0.0
+frame_ys = zeros((2, length(frame_time)))
+for i in 1:length(frame_time)-1
+    frame_ys[:,i+1] = update(frame_ys[:,i])
+end
+
+
+graph_canvas = Scene(resolution=(400,800), backgroundcolor = bg_col)
+plotted_line1 = lines!(graph_canvas, frame_start .+ frame_time, frame_ys[1,:], color=:white)
+plotted_line1 = plotted_line1[end]
+plotted_line2 = lines!(graph_canvas, frame_start .+ frame_time, frame_ys[2,:], color=:goldenrod)
+plotted_line2 = plotted_line2[end]
+
+lineplots = [plotted_line1, plotted_line2]
+
+# Updates colors
+for axis in [particles_canvas[Axis], graph_canvas[Axis]]
+    axis[:grid, :linewidth] =  (1, 1)
+    axis[:grid, :linecolor] = (RGBA{Float32}(0.5, 0.7, 1.0, 0.5),RGBA{Float32}(0.5, 0.7, 1.0, 0.5))
+    axis[:names][:textsize] = (0.0,0.0)
+    axis[:ticks, :textcolor] = (RGBA{Float32}(0.5, 0.7, 1.0, 0.8),RGBA{Float32}(0.5, 0.7, 1.0, 0.8))
+end
+
+
+# Define combined canvas
+parent = Scene(resolution = (1400, 800))
+vbox(hbox(s1, s2, s3, s4, s5, s6), particles_canvas, graph_canvas, parent = parent)
 display(parent)
 
 
 
-update!(x, y, jump = indentity) = update!(NaN, x, y, jump)
+
+
 sleep(1)
 N = 4000
 jump = rebirth(0.0001, R)
+c = 1
+
 for i in 1:N
     global c
+    global frame_start
     cnew = mod1(c+1, trace_len)
     update!(x[c][], x[cnew][], jump)
     c = cnew
@@ -87,11 +117,20 @@ for i in 1:N
         f = (i + 5*(i!=0))/(trace_len+5)
         col[mod1(c-i, trace_len)][] = RGBA{Float32}(0.5, 0.7, 1.0, (1-sqrt(f))/3)
     end
+
+    frame_ys[:,1:end-1] .= frame_ys[:,2:end]
+    frame_ys[:,end] = update(frame_ys[:,end-1])
+    for (l,j) in zip(lineplots, [1,2])
+        l[1] = frame_start .+ frame_time
+        l[2] = frame_ys[j,:]
+    end
+    AbstractPlotting.update!(graph_canvas)
+
     sleep(1e-10)
 end
 
 if RECORD
-record(p, "output/fitzhugh.mp4", 1:(N÷10)) do i
+record(parent, "output/fitzhugh.mp4", 1:(N÷10)) do i
     global c
     cnew = mod1(c+1, trace_len)
     update!(x[c][], x[cnew][], jump)
@@ -102,6 +141,15 @@ record(p, "output/fitzhugh.mp4", 1:(N÷10)) do i
         f = (i + 5*(i!=0))/(trace_len+5)
         col[mod1(c-i, trace_len)][] = RGBA{Float32}(0.5, 0.7, 1.0, (1-sqrt(f))/3)
     end
+
+    frame_ys[:,1:end-1] .= frame_ys[:,2:end]
+    frame_ys[:,end] = update(frame_ys[:,end-1])
+    for (l,j) in zip(lineplots, [1,2])
+        l[1] = frame_start .+ frame_time
+        l[2] = frame_ys[j,:]
+    end
+    AbstractPlotting.update!(graph_canvas)
+
     yield()
 end
 end
