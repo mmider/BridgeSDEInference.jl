03a70ae186522ed137056105abdf4e3471a84a26
Marcin <marcin.mider@gmail.com>
2019-10-26 10:27:35 +0200

    removed silly typos

--- scripts/data_generation/prokaryotic_gillespie.jl
+++ scripts/data_generation/prokaryotic_gillespie.jl
@@ -1,44 +1,12 @@
 using DiffEqBiological
 
-#NOTE I think this will not work, because to all equations are of the
-# Mass action reaction type.
-function simulate_data_deprecated(rates, prob)
-    reactant_stoich =
-    [
-        [4 => 1, 3 => 1],   # 1*DNA + 1*P₂
-        [5 => 1],           # 1*DNA.P₂
-        [4 => 1],           # 1*DNA
-        [1 => 1],           # 1*RNA
-        [2 => 1],           # 2*P
-        [3 => 1],           # 1*P₂
-        [1 => 1],           # 1*RNA
-        [2 => 1]            # 1*P
-    ]
-    net_stoich =
-    [
-        [5 => 1],           # 1*DNA.P₂
-        [4 => 1, 3 => 1],   # 1*DNA + 1*P₂
-        [4 => 1, 1 => 1],   # 1*DNA + 1*RNA
-        [1 => 1, 2 => 1],   # 1*RNA + 1*P
-        [3 => 1],           # 1*P₂
-        [2 => 2],           # 2*P
-        [1 => 0],           # 0
-        [2 => 0]            # 0
-    ]
-    mass_act_jump = MassActionJump(rates, reactant_stoich, net_stoich)
-    jump_prob = JumpProblem(prob, Direct(), mass_act_jump)
-    sol = solve(jump_prob, SSAStepper())
-    sol
-end
-
-
 prokaryote_model = @reaction_network ProkaryoteRN begin
     θ1, DNA + P₂ --> DNAxP₂
     θ2, DNAxP₂ --> DNA + P₂
-    θ3, DNA --> DNA + P₂
+    θ3, DNA --> DNA + RNA
     θ4, RNA --> RNA + P
     θ5, 2P --> P₂
-    θ6, P₂ --> 2*P₂
+    θ6, P₂ --> 2*P
     θ7, RNA --> 0*RNA
     θ8, P --> 0*P
 end θ1 θ2 θ3 θ4 θ5 θ6 θ7 θ8
@@ -56,7 +24,6 @@ u0 = [8,8,8,5,5]
 tt = (0.0, 100.0)
 
 
-#NOTE this halts my departmental computer, pending repeat at home...
 sol = simulate_data(u0, tt, θ)
 
 using Plots; plot(sol)
