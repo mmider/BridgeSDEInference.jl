060d8fd580e36a2439177de70c329a2128f6e3ca
mschauer <moritzschauer@web.de>
2020-02-01 11:18:45 +0100

    Document script

--- scripts/inference/lotka_volterra.jl
+++ scripts/inference/lotka_volterra.jl
@@ -1,63 +1,106 @@
-SRC_DIR = joinpath(Base.source_dir(), "..", "..", "src")
-OUT_DIR = joinpath(Base.source_dir(), "..", "..", "output")
-mkpath(OUT_DIR)
-
-
-#include(joinpath(SRC_DIR, "BridgeSDEInference_for_tests.jl"))
-
 using StaticArrays
 using Distributions
 using Random
-# Let's generate the data
-# -----------------------
 using Bridge
 using Statistics, LinearAlgebra
 using GaussianDistributions
 using BridgeSDEInference
-using BridgeSDEInference: LotkaVolterraDiffusion, LotkaVolterraDiffusionAux
+const SDE = BridgeSDEInference
+
+
+## Helper functions and directories
+
+SRC_DIR = joinpath(Base.source_dir(), "..", "..", "src")
+OUT_DIR = joinpath(Base.source_dir(), "..", "..", "output")
+mkpath(OUT_DIR)
+
 DIR = "auxiliary"
 include(joinpath(SRC_DIR, DIR, "data_simulation_fns.jl"))
 include(joinpath(SRC_DIR, DIR, "utility_functions.jl"))
-Random.seed!(4)
-#θ₀ = [10.0, 28.0, 8.0/3.0, 3.0, 3.0, 3.0]
-const SDE = BridgeSDEInference
 
-θˣ = [1.5, 1.0, 3.0, 1.0, 0.2, 0.2]
+
+### Import example SDE LotkaVolterraDiffusion
+
+using BridgeSDEInference: LotkaVolterraDiffusion, LotkaVolterraDiffusionAux
+
+#=
+Stochastic Lotka-Volterra System (SDE) with
+parameters (α, β, γ, δ, σ1, σ2) with drift
+
+     b(x) = [α*x[1] - β*x[1]*x[2],
+             δ*x[1]*x[2] - γ*x[2]]
+
+and volatility
+
+    σ(x) = [ σ1 0
+             0  σ2 ]
+
+# parameters: see src/examples/lotka_volterra.jl
+=#
+
+
+### Generate data with parameters
+
+Random.seed!(4)
+θˣ = [1.5, 1.0, 3.0, 1.0, 0.2, 0.2] #  (α, β, γ, δ, σ1, σ2)
 Pˣ = LotkaVolterraDiffusion(θˣ...)
 
-x0, dt, T = ℝ{2}(2.5, 2.), 1/5000, 20.0
+x0 = ℝ{2}(2.5, 2.) # starting point
+w0 = ℝ{2}(0.0, 0.0) # starting point of driving Wiener process
+
+dt, T =  1/5000, 20.0 # time grid
 tt = 0.0:dt:T
 
-XX, _ = simulate_segment(ℝ{2}(0.0, 0.0), x0, Pˣ, tt)
+XX, _ = simulate_segment(w0, x0, Pˣ, tt) # simulate process
 
+
+### Subsampling observations
+
+skip = 2000
+
+# Set observation scheme: partial observations of first coordinate
+# with observation operator L and Gaussian noise
+Σdiagel = 1.0
+Σ = @SMatrix[Σdiagel]
+L = @SMatrix[1.0 0.0]
+
+# Observe with noise at sample times
+obs_time, obs_vals = XX.tt[1:skip:end], [rand(Gaussian(L*x, Σ)) for x in XX.yy[1:skip:end]]
+
+
+### Setup MCMC scheme to infer parameters given observations
 
 θ_init = copy(θˣ)
 Pˣ = LotkaVolterraDiffusion(θ_init...)
 
-skip = 2000
-
-Σdiagel = 10.0^0
-Σ = @SMatrix[Σdiagel]#SMatrix{2,2}(1.0I)*Σdiagel
-L = @SMatrix[1.0 0.0]
-
-obs_time, obs_vals = XX.tt[1:skip:end], [rand(Gaussian(L*x, Σ)) for x in XX.yy[1:skip:end]]
-
+# Setup: For observation intervals define linearizations of the diffusion to be used
+# in generating MCMC proposals
 P̃ = [LotkaVolterraDiffusionAux(θ_init..., t₀, u, T, v) for (t₀, T, u, v)
      in zip(obs_time[1:end-1], obs_time[2:end], obs_vals[1:end-1], obs_vals[2:end])]
 
+# Setup: Partial observations: register observation scheme of each observation
 model_setup = DiffusionSetup(Pˣ, P̃, PartObs())
-set_observations!(model_setup, [L for _ in P̃], [Σ for _ in P̃], obs_vals, obs_time) # uses default fpt
+set_observations!(model_setup, [L for _ in P̃], [Σ for _ in P̃], obs_vals, obs_time)
 set_imputation_grid!(model_setup, 1/1000)
+
+# Setup: Priors
 set_x0_prior!(model_setup,
               GsnStartingPt(x0, @SMatrix [20.0 0.0;
                                           0.0 20.0;]),
               x0)
+
+# Initialize sampler
 set_auxiliary!(model_setup; skip_for_save=10^0,
                adaptive_prop=NoAdaptation())
 initialise!(eltype(x0), model_setup, Vern7(), false, NoChangePt(100))
 
+# Parameters for adaptively finding good Metropolis stepsize
 readj = (100, 0.001, 0.001, 999.9, 0.234, 50)
 
+# Finalize setup:
+# Define MCMC steps for 1.) path imputation, 2.) conjugate updates for
+# linear parameters α, β, γ, δ and 3.) - 8.) random walk steps
+# for parameters α, β, γ, δ, σ1, σ2 (in that order)
 mcmc_setup = MCMCSetup(
       Imputation(NoBlocking(), 0.99, Vern7()),
       ParamUpdate(ConjugateUpdt(), [1,2,3,4], θ_init, nothing,
@@ -82,17 +125,24 @@ mcmc_setup = MCMCSetup(
                   UniformRandomWalk(0.1, true), ImproperPosPrior(),
                   UpdtAuxiliary(Vern7(), check_if_recompute_ODEs(P̃, 6)), readj))
 
-schedule = MCMCSchedule(1*10^4, [[1],[3,4,6]],
+# Set up schedule for MCMC: Impute, and estimate parameters α, β, and δ (Steps 1, 3, 4, 6)
+schedule = MCMCSchedule(1*10^3, [[1],[3,4,6]], # which (groups of) steps in which order
                         (save=1*10^3, verbose=10^2, warm_up=100,
                          readjust=(x->x%100==0), fuse=(x->false)))
 
+
+### Run MCMC
+
 Random.seed!(4)
-out = mcmc(mcmc_setup, schedule, model_setup)
-ws = out[2]
+state, history = mcmc(mcmc_setup, schedule, model_setup)
+
+# most importantly: parameter samples
+θs = history.θ_chain
+
+### Post processing: plot results
+
 include(joinpath(SRC_DIR, DIR, "plotting_fns.jl"))
-plot_chains(ws; truth=θˣ)
-#=
-plot_paths(out[1], out[2], schedule; obs=(times=obs_time[2:end],
-                     vals=[[v[1] for v in obs_vals[2:end]],
-                           [v[2] for v in obs_vals[2:end]]], indices=[2,3]))
-=#
+plot_chains(history; truth=θˣ)
+
+plot_paths(state, history, schedule; obs=(times=obs_time[2:end],
+                     vals=[[v[1] for v in obs_vals[2:end]]], indices=[1]))
