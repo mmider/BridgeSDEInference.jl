5c91968d63fa3b1848574ccf4b02a426778fa5c2
SebaGraz <sebastiano.grazzi@gmail.com>
2020-03-11 15:27:22 +0100

    Fix sir diffusion and auxiliary diffusion

--- src/examples/sir.jl
+++ src/examples/sir.jl
@@ -9,18 +9,17 @@ struct SIR{T} <: ContinuousTimeProcess{SVector{2,T}}
     β::T
     σ1::T
     σ2::T
-    k::T
 end
 
-b(t, u, P::SIR) = @SVector [P.α*(P.k - u[1] - u[2])*u[1] - P.β*u[1], P.β*u[1]]
+b(t, u, P::SIR) = @SVector [P.α*(1 - u[1] - u[2])*u[1] - P.β*u[1], P.β*u[1]]
 σ(t, u, P::SIR) = @SMatrix Float64[
-    -P.σ1*sq((P.k - u[1] - u[2])*u[1])  -P.σ2*sq(u[1])
+    -P.σ1*sq((1 - u[1] - u[2])*u[1])  -P.σ2*sq(u[1])
                 0.0   P.σ2*sq.(u[1])
     ]
 a(t, u, P::SIR) = σ(t, u, P)*σ(t, u, P)'
 constdiff(::SIR) = false
 clone(P::SIR, θ) = SIR(θ...)
-params(P::SIR) = [P.α, P.β, P.σ1, P.σ2, P.k]
+params(P::SIR) = [P.α, P.β, P.σ1, P.σ2]
 #domain(P::SIR) = LowerBoundedDomain((0.0, 0.0), (1,2))
 domain(P::SIR) = LowerBoundedDomain((0.0,), (1,))
 
@@ -47,7 +46,6 @@ struct SIRAux{T,S1,S2} <: ContinuousTimeProcess{SVector{2,T}}
     β::T
     σ1::T
     σ2::T
-    k::T
     t::Float64
     u::S1
     T::Float64
@@ -55,23 +53,42 @@ struct SIRAux{T,S1,S2} <: ContinuousTimeProcess{SVector{2,T}}
 end
 
 
+# function B(t, P::SIRAux)
+# #    b(t, u, P::SIR) = @SVector [P.α*(P.k - u[1] - u[2])*u[1] - P.β*u[1], P.β*u[1]]
+#     @SMatrix [(P.α) -(P.β);
+#               (P.β) -0.0]
+# end
 function B(t, P::SIRAux)
-#    b(t, u, P::SIR) = @SVector [P.α*(P.k - u[1] - u[2])*u[1] - P.β*u[1], P.β*u[1]]
-    @SMatrix [(P.α) -(P.β);
-              (P.β) -0.0]
+#    b(t, u, P::SIR) = @SVector [P.α*(1 - u[1] - u[2])*u[1] - P.β*u[1], P.β*u[1]]
+    @SMatrix [(P.α*(1 - P.v[1] - P.v[2]) - (P.β))  0.0;
+              (P.β) 0.0]
 end
+
+
+
+
 
 # mean = ℝ{2}(P.γ/P.δ, P.α/P.β)
 function β(t, P::SIRAux)
     ℝ{2}(0.0, 0.0)
 end
 
+# function σ(t, P::SIRAux)
+#     sq(0.0001)*@SMatrix Float64[
+#          -P.σ1  -P.σ2
+#           0.0   P.σ2
+#         ]
+# end
+
 function σ(t, P::SIRAux)
-    sq(0.0001)*@SMatrix Float64[
-         -P.σ1  -P.σ2
-          0.0   P.σ2
-        ]
+        @SMatrix Float64[
+         (-P.σ1*(1 - P.v[2] - P.v[1])*P.v[1])  -P.σ2*P.v[1];
+         0.0   P.σ2*P.v[1]]
 end
+
+
+
+
 σ(t, x, P::SIRAux) = σ(t, P)
 
 depends_on_params(::SIRAux) = (3, 4, 5)
@@ -84,4 +101,4 @@ a(t, P::SIRAux) = σ(t,P) * σ(t, P)'
 clone(P::SIRAux, θ) = SIRAux(θ..., P.t, P.u, P.T, P.v)
 
 clone(P::SIRAux, θ, v) = SIRAux(θ..., P.t, v, P.T, v)
-params(P::SIRAux) = [P.α, P.β, P.σ1, P.σ2, P.k]
+params(P::SIRAux) = [P.α, P.β, P.σ1, P.σ2]
