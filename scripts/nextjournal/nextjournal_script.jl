ea77f7a21b5158cd9d67d365b41b74246324a7fc
mschauer <moritzschauer@web.de>
2020-03-26 12:08:10 +0100

    Fixes

--- scripts/nextjournal/nextjournal_script.jl
+++ scripts/nextjournal/nextjournal_script.jl
@@ -1,10 +1,11 @@
 ## Intro
+using Profile, Revise
 using Bridge
 using StaticArrays
 using BridgeSDEInference
 using Random, LinearAlgebra, Distributions
 const State = SArray{Tuple{2},T,1,2} where {T};
-
+using BridgeSDEInference: EulerMaruyamaBounded
 param = :regular # regular parametrization of the trajectories (no transformation of the coordinate processes)
 ε = 0.1 ; s =-0.8 ; γ =1.5 ; β = 0.0 ; σ =0.3 ;
 P = FitzhughDiffusion(param, ε, s, γ, β, σ);
@@ -27,22 +28,19 @@ num_obs = 100
 skip = div(length(tt), num_obs)
 Σ = [10^(-4)]
 L = [1.0 0.0]
-obs = (time = X.tt[1:skip:end],
-values = [Float64(rand(MvNormal(L*x, Σ))[1]) for x in X.yy[1:skip:end]]);
+obs = (time = X.tt[1:skip:end], values = [Float64(rand(MvNormal(L*x, Σ))[1]) for x in X.yy[1:skip:end]]);
 
 ## Plotting
-using Plots
+#=using Plots
 gr()
 Plots.plot(X.tt,  first.(X.yy), label = "X")
 Plots.plot!(X.tt,  last.(X.yy), label = "Y")
 Plots.scatter!(obs.time, obs.values, markersize=1.5, label = "observations")
-
+=#
 ## Inference
-θ_init = [ε, s, γ, β, σ].*(1 .+ randn(5).*[0.5, 0.5, 0.5, 0.0, 0.5])
-# Verify that the parameters lay in their domain
-θ_init[1]<0 ? θ_init[1] = -θ_init[1] : θ_init[1] = θ_init[1]
-θ_init[3]<0 ? θ_init[3] = -θ_init[3]  : θ_init[3] = θ_init[3]
-θ_init[5]<0 ? θ_init[5] = -θ_init[5] : θ_init[5] = θ_init[5];
+θ_init = [ε, s, γ, β, σ].*(1 .+ (2*rand(5) .- 1).*[0.2, 0.2, 0.2, 0.0, 0.2])
+
+
 # Take the real β, as it is fixed.
 
 P_trgt = FitzhughDiffusion(param, θ_init...)
@@ -50,7 +48,20 @@ P_aux = [FitzhughDiffusionAux(param, θ_init..., t₀, u, T, v) for (t₀,T,u,v)
         in zip(obs.time[1:end-1], obs.time[2:end], obs.values[1:end-1], obs.values[2:end])]
 
 # Container
-model_setup = DiffusionSetup(P_trgt, P_aux, PartObs());
+model_setup = DiffusionSetup(P_trgt, P_aux, PartObs())
+
+# Observation scheme
+L = @SMatrix [1. 0.]
+Σ = @SMatrix [10^(-4)]
+set_observations!(model_setup, [L for _ in P_aux], [Σ for _ in P_aux],
+                  obs.values, obs.time)
+
+# Imputation grid
+dt = 1/200
+set_imputation_grid!(model_setup, dt)
+# Prior distribution on (X_0, Y_0)
+set_x0_prior!(model_setup, KnownStartingPt(x0));
+#set_x0_prior!(model_setup, GsnStartingPt(x0, @SMatrix [.1 0; 0 .1]), x0);
 
 initialise!(eltype(x0), model_setup, Vern7(), false, NoChangePt(100))
 # Further setting
@@ -80,6 +91,5 @@ schedule = MCMCSchedule(1*10^3, [[1,2,3,4,5]],
                          readjust=(x->x%100==0), fuse=(x->false)));
 
 Random.seed!(4)
-out = mcmc(mcmc_setup, schedule, model_setup);
-
-                         
+Profile.init()
+@profile out = mcmc(mcmc_setup, schedule, model_setup);
